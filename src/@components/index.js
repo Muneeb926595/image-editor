13cf4cb44c9f00310ee49ae2455313afb81e554f
Muneeb926595 <muneebkhann0@gmail.com>
2021-06-13 16:17:44 +0500

    Cleaned Components

--- src/@components/index.js
+++ src/@components/index.js
@@ -1,6 +1,5 @@
 export { default as Avatar } from "./Avatar";
 export { default as Icon } from "./Icon";
 export { default as Clickable } from "./Clickable";
-export { default as Button } from "./Button";
 export { default as Text } from "./Text";
 export { default as Input } from "./Input";
--- src/@styles/index.js
+++ src/@styles/index.js
@@ -1,4 +1,3 @@
 export { default as inputStyles } from "./inputStyles";
-export { default as buttonStyles } from "./buttonStyles";
 export { default as textStyles } from "./textStyles";
 export { default as theme } from "./theme";
