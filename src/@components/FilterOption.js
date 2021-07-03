ba5fbfc141adf2ba22e799d2c2ced20216a1211b
Muneeb926595 <muneebkhann0@gmail.com>
2021-07-03 14:48:08 +0500

    Added Filters Data

--- src/@components/FilterOption.js
+++ src/@components/FilterOption.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const EditorOption = ({ optionName, handleClick }) => {
+const EditorOption = ({ singleFilter, handleClick }) => {
   return (
     <div className="flex flex-col justify-center items-center mr-4">
       <div className="flex justify-center items-center cursor-pointer h-24 w-30">
@@ -10,7 +10,9 @@ const EditorOption = ({ optionName, handleClick }) => {
           src="https://images.unsplash.com/photo-1624779942936-02e331404c01?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
         />
       </div>
-      <p className="text-gray-400 mt-2 font-serif text-sm ">{optionName}</p>
+      <p className="text-gray-400 mt-2 font-serif text-sm ">
+        {singleFilter.optionName}
+      </p>
     </div>
   );
 };
